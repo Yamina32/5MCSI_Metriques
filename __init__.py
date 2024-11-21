190ecd2d0552113654742ca6974a9f9debc48dc5
Yamina32 <146001394+Yamina32@users.noreply.github.com>
2024-11-21 16:23:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
  @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"  
-  
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
